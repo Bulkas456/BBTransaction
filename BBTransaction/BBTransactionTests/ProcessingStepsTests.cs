23ddfcd60bf198548b7261a22a7d4b72f76a63db
Bulkas456 <bulkas@interia.pl>
2018-01-04 09:57:55 +0100

    Added tests projects.

---
+++ BBTransaction/BBTransactionTests/ProcessingStepsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BBTransactionTestsNetCore
+{
+    [TestClass]
+    public class ProcessingStepsTests
+    {
+        [TestMethod]
+        public void WhenRunTransaction_ShouldRunAllSteps()
+        {
+        }
+    }
+}
---
+++ BBTransaction/BBTransactionTestsNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BBTransactionTestsNetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acer")]
+[assembly: AssemblyProduct("BBTransactionTestsNetFramework")]
+[assembly: AssemblyCopyright("Copyright © Acer 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4cc870f6-94a9-4e82-8b30-003cd4eabd9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
