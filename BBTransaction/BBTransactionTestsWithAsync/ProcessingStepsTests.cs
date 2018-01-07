b2f1978664515d14b3e80ae33990825dceda04d4
Bulkas456 <bulkas@interia.pl>
2018-01-07 11:26:15 +0100

    Unit test for SameExecutorForAllActions setting.

--- BBTransaction/BBTransactionTestsWithAsync/ProcessingStepsTests.cs
+++ BBTransaction/BBTransactionTestsWithAsync/ProcessingStepsTests.cs
@@ -15,6 +15,7 @@ using BBTransaction.Step.Executor;
 using System.Threading;
 using System.Linq;
 using BBTransactionTestsWithAsync;
+using BBTransaction.Step.Settings;
 
 namespace BBTransactionTestsNetCore
 {
@@ -323,25 +324,201 @@ namespace BBTransactionTestsNetCore
             transactionCallbackThreadId.Should().Be(step3PostActionThreadId);
         }
 
-        private Mock<IStepExecutor> CreateStepExecutorMock(Action<int> threadIdGetAction)
+        [TestMethod]
+        public async Task WhenRunTransactionWithStepExecutorsWithSameExecutorForAllActions_ShouldRunAllStepsProperly()
         {
-            Mock<IStepExecutor> mock = new Mock<IStepExecutor>();
-            mock.SetupGet(x => x.ShouldRun)
-                .Returns(true);
-            mock.Setup(x => x.Run(It.IsNotNull<Func<Task>>()))
-                .Callback((Func<Task> method) => 
+            // Arrange
+            object transactionData = new object();
+            List<string> runStepActions = new List<string>();
+            List<string> runUndoActions = new List<string>();
+            List<string> runPostActions = new List<string>();
+            List<int> stepActionThreadId = new List<int>();
+            List<int> postActionThreadId = new List<int>();
+            int transactionCallbackThreadId = 0;
+            Dictionary<int, TestExecutor> stepExecutors = new Dictionary<int, TestExecutor>()
+            {
+                { 0, null },
+                { 1, new TestExecutor() { ShouldRun = true } },
+                { 2, null },
+                { 3, new TestExecutor() { ShouldRun = true } },
+                { 4, new TestExecutor() { ShouldRun = true }},
+                { 5, null },
+                { 6, null }
+            };
+            Dictionary<int, TestExecutor> postExecutors = new Dictionary<int, TestExecutor>()
+            {
+                { 0, null },
+                { 1, null },
+                { 2, new TestExecutor() { ShouldRun = true } },
+                { 3, new TestExecutor() { ShouldRun = true } },
+                { 4, null },
+                { 5, null },
+                { 6, null }
+            };
+            int transactionRunThreadId = Thread.CurrentThread.ManagedThreadId;
+            int step1StepActionThreadId = stepExecutors[1].ThreadId;
+            int step2PostActionThreadId = postExecutors[2].ThreadId;
+            int step3StepActionThreadId = stepExecutors[3].ThreadId;
+            int step3PostActionThreadId = postExecutors[3].ThreadId;
+            int step4StepActionThreadId = stepExecutors[4].ThreadId;
+            ITransaction<string, object> target = new TransactionFactory().Create<string, object>(options =>
+            {
+                options.TransactionInfo.Name = "test transaction";
+            });
+
+            for (int i = 0; i < 7; ++i)
+            {
+                string index = i.ToString();
+                TransactionStep<string, object> step = new TransactionStep<string, object>()
                 {
-                    new Thread(new ThreadStart(() =>
+                    Id = index,
+                    StepActionExecutor = stepExecutors[i] == null
+                                           ? null
+                                           : stepExecutors[i],
+                    PostActionExecutor = postExecutors[i] == null
+                                           ? null
+                                           : postExecutors[i],
+                    Settings = i == 1
+                                ? StepSettings.SameExecutorForAllActions
+                                : StepSettings.None
+                };
+
+                if (i == 3
+                    || i == 4)
+                {
+                    step.AsyncStepAction = async (data, info) =>
                     {
-                        threadIdGetAction(Thread.CurrentThread.ManagedThreadId);
-                        method();
-                    }))
+                        data.Should().BeSameAs(transactionData);
+                        info.CurrentStepId.Should().Be(index);
+                        runStepActions.Add(index);
+                        stepActionThreadId.Add(Thread.CurrentThread.ManagedThreadId);
+                        await Task.CompletedTask;
+                    };
+                }
+                else
+                {
+                    step.StepAction = (data, info) =>
                     {
-                        IsBackground = true
-                    }
-                    .Start();
-                });
-            return mock;
+                        data.Should().BeSameAs(transactionData);
+                        info.CurrentStepId.Should().Be(index);
+                        runStepActions.Add(index);
+                        stepActionThreadId.Add(Thread.CurrentThread.ManagedThreadId);
+                    };
+                }
+
+                if (i % 4 == 0)
+                {
+                    step.AsyncUndoAction = async (data, info) =>
+                    {
+                        data.Should().BeSameAs(transactionData);
+                        info.CurrentStepId.Should().Be(index);
+                        runUndoActions.Add(index);
+                        await Task.CompletedTask;
+                    };
+                }
+                else
+                {
+                    step.UndoAction = (data, info) =>
+                    {
+                        data.Should().BeSameAs(transactionData);
+                        info.CurrentStepId.Should().Be(index);
+                        runUndoActions.Add(index);
+                    };
+                }
+
+                if (i == 1
+                    || i == 2)
+                {
+                    step.AsyncPostAction = async (data) =>
+                    {
+                        data.Should().BeSameAs(transactionData);
+                        runPostActions.Add(index);
+                        postActionThreadId.Add(Thread.CurrentThread.ManagedThreadId);
+                        await Task.CompletedTask;
+                    };
+                }
+                else
+                {
+                    step.PostAction = (data) =>
+                    {
+                        data.Should().BeSameAs(transactionData);
+                        runPostActions.Add(index);
+                        postActionThreadId.Add(Thread.CurrentThread.ManagedThreadId);
+                    };
+                }
+
+                target.Add(step);
+            }
+            ITransactionResult<object> transactionCallbackResult = null;
+
+            /*
+             0:
+             1: executor for other thread (step action), SameExecutorForAllActions
+             2: executor for other thread (post action)
+             3: executor for other thread (step action), executor for other thread (post action)
+             4: executor for other thread (step action)
+             5: 
+             6: 
+             */
+
+            // Act
+            ITransactionResult<object> result = await target.Run(settings =>
+            {
+                settings.Data = transactionData;
+                settings.Mode = RunMode.Run;
+                settings.TransactionResultCallback = callbackResult =>
+                {
+                    transactionCallbackThreadId = Thread.CurrentThread.ManagedThreadId;
+                    transactionCallbackResult = callbackResult;
+                };
+            });
+
+            // Assert
+            foreach (TestExecutor executor in stepExecutors.Values.Concat(postExecutors.Values).Where(x => x != null))
+            {
+                executor.Dispose();
+            }
+
+            result.Should().BeSameAs(transactionCallbackResult);
+            result.Data.Should().BeSameAs(transactionData);
+            result.Errors.ShouldAllBeEquivalentTo(new Exception[0]);
+            result.Info.Should().Be(string.Empty);
+            result.Recovered.Should().BeFalse();
+            result.Success.Should().BeTrue();
+            TestExecutor sharedExecutor = stepExecutors[1];
+            sharedExecutor.Mock.VerifyGet(x => x.ShouldRun, Times.Exactly(2));
+            sharedExecutor.Mock.Verify(x => x.Run(It.IsNotNull<Func<Task>>()), Times.Exactly(2));
+
+            foreach (TestExecutor executor in stepExecutors.Values.Concat(postExecutors.Values).Where(x => x != null && x != sharedExecutor))
+            {
+                executor.Mock.VerifyGet(x => x.ShouldRun, Times.Once);
+                executor.Mock.Verify(x => x.Run(It.IsNotNull<Func<Task>>()), Times.Once);
+            }
+
+            runStepActions.ShouldAllBeEquivalentTo(new string[] { "0", "1", "2", "3", "4", "5", "6" });
+            runUndoActions.ShouldAllBeEquivalentTo(new string[0]);
+            runPostActions.ShouldAllBeEquivalentTo(new string[] { "0", "1", "2", "3", "4", "5", "6" });
+            stepActionThreadId.ShouldAllBeEquivalentTo(new int[]
+            {
+                /*step 0*/ transactionRunThreadId,
+                /*step 1*/ step1StepActionThreadId,
+                /*step 2*/ step1StepActionThreadId,
+                /*step 3*/ step3StepActionThreadId,
+                /*step 4*/ step4StepActionThreadId,
+                /*step 5*/ step4StepActionThreadId,
+                /*step 6*/ step4StepActionThreadId
+            });
+            postActionThreadId.ShouldAllBeEquivalentTo(new int[]
+            {
+                /*step 0*/ step4StepActionThreadId,
+                /*step 1*/ step1StepActionThreadId,
+                /*step 2*/ step2PostActionThreadId,
+                /*step 3*/ step3PostActionThreadId,
+                /*step 4*/ step3PostActionThreadId,
+                /*step 5*/ step3PostActionThreadId,
+                /*step 6*/ step3PostActionThreadId
+            });
+            transactionCallbackThreadId.Should().Be(step3PostActionThreadId);
         }
     }
 }
